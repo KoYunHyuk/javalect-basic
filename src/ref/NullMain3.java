06f792748d444b6e27803f5eaf86d67c55f48936
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-09-29 19:09:48 +0900

    Null and NullPointerException - Solving Problem

--- src/ref/NullMain3.java
+++ src/ref/NullMain3.java
@@ -1,11 +1,16 @@
 package ref;
 
+import java.util.Scanner;
+
 public class NullMain3 {
     public static void main(String[] args) {
         BigData bigData = new BigData();
         bigData.data = new Data();
         System.out.println("bitData.count=" + bigData.count); // 0
         System.out.println("bitData.data="+bigData.data); // ref.Data@27bc2616
+
+        System.out.println("bigData.data.value" + bigData.data.value); // 0
+       
 
         System.out.println("bigData.data.value" + bigData.data.value); // 0
     }
