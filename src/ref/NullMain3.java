5cc7a04aafd747d9ebbc63565adbb476da327f89
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-09-29 19:10:22 +0900

    Null and NullPointerException - Solving Problem

--- src/ref/NullMain3.java
+++ src/ref/NullMain3.java
@@ -10,8 +10,5 @@ public class NullMain3 {
         System.out.println("bitData.data="+bigData.data); // ref.Data@27bc2616
 
         System.out.println("bigData.data.value" + bigData.data.value); // 0
-       
-
-        System.out.println("bigData.data.value" + bigData.data.value); // 0
     }
 }
