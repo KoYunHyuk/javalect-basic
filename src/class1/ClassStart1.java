116a65865fce62adeef942d941ac8aeda2b3e84a
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-09-29 16:38:02 +0900

    Class Definition and Application

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,14 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+
+        student1.name = "혁";
+        student1.age = 10;
+        student1.grade = 20;
+
+        System.out.println(student1.name + student1.grade + student1.age);
+    }
+
+}
